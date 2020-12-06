37f864cbba578962f7bae06c773ea1142e4aa281
Kentaro Yoshizumi <kentaroyoshizumi@mbp.local>
2020-12-06 14:52:29 +0900

    implement eventsIndex

---
+++ src/components/events_index.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { readEvents } from '../actions'
+import { connect } from 'react-redux'
+import _ from 'lodash'
+
+
+class EventsIndex extends Component {
+  componentDidMount() {
+    this.props.readEvents()
+  }
+
+  renderEvents() {
+    return _.map(this.props.events, event => (
+      <tr key={event.id}>
+        <td>{event.id}</td>
+        <td>{event.title}</td>
+        <td>{event.body}</td>
+      </tr>
+    ))
+  }
+
+  render() {
+    
+
+    return (
+
+       <table>
+         <thread>
+           <tr>
+             <th>ID</th>
+             <th>Title</th>
+             <th>Body</th>
+           </tr>
+         </thread>
+
+         <tbody>
+           {this.renderEvents()}
+         </tbody>
+       </table>
+      
+      )
+    }
+  }
+
+  const mapStateToProps = state => ({ events: state.events })
+
+  const mapDispatchToProps = ({ readEvents })
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex)
--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+
 import './index.css';
 import reducer from './reducers'
-import App from './components/App.js';
+import EventsIndex from './components/events_index';
+
+import registerServiceWorker from './registerServiceWorker'
 import reportWebVitals from './reportWebVitals';
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
   <React.StrictMode>
-    <App />
+    <EventsIndex />
   </React.StrictMode>
   </Provider>,
   document.getElementById('root')
