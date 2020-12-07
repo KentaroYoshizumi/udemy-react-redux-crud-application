47f8a532832fde1311f5a91bf3500b3077835ef9
Kentaro Yoshizumi <kentaroyoshizumi@mbp.local>
2020-12-07 11:24:09 +0900

    implement event new component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux'
-import count from './events'
+import { reducer as form } from 'redux-form'
+import events from './events'
 
-export default combineReducers({ events })
+export default combineReducers({ events, form })
